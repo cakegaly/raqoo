d1a46a7f76fdd3309dceb87f4c920f328cd2c402
Toshiki Okazaki <toshiki.okazaki@gree.net>
2024-12-07 19:40:51 +0900

    feat: add reactflow & display simple diagrams

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,19 @@
-import ReactSVG from "@/assets/react.svg";
-import CountBtn from "@/components/count-btn";
-import { Badge } from "@/components/ui/badge";
+import ReactFlow, { Background } from "reactflow";
+import "reactflow/dist/style.css";
 
 function App() {
+  const nodes = [
+    { id: "1", position: { x: 250, y: 5 }, data: { label: "Node 1" } },
+    { id: "2", position: { x: 100, y: 100 }, data: { label: "Node 2" } },
+  ];
+
+  const edges = [{ id: "e1-2", source: "1", target: "2" }];
+
   return (
     <main className="flex flex-col items-center justify-center h-screen">
-      <div className="flex flex-col items-center gap-y-4">
-        <div className="inline-flex items-center gap-x-4">
-          <img src={ReactSVG} alt="React Logo" className="w-32" />
-          <span className="text-6xl">+</span>
-          <img src={"/vite.svg"} alt="Vite Logo" className="w-32" />
-        </div>
-        <a
-          href="https://ui.shadcn.com"
-          rel="noopener noreferrer nofollow"
-          target="_blank"
-        >
-          <Badge variant="outline">shadcn/ui</Badge>
-        </a>
-        <CountBtn />
-      </div>
+      <ReactFlow nodes={nodes} edges={edges}>
+        <Background />
+      </ReactFlow>
     </main>
   );
 }
