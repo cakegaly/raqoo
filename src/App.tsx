981776d16587ec085c8e02a39ce4b8ef116ff827
Toshiki Okazaki <toshiki.okazaki@gree.net>
2024-12-07 20:36:04 +0900

    feat: update manage button of node & edge

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
-import { useCallback } from "react";
-import ReactFlow, { Background, useEdgesState, useNodesState } from "reactflow";
+import { useCallback, useState } from "react";
+import ReactFlow, {
+  Background,
+  Node,
+  NodeMouseHandler,
+  useEdgesState,
+  useNodesState,
+} from "reactflow";
 import "reactflow/dist/style.css";
 
 let id = 2;
@@ -13,46 +19,49 @@ function App() {
     { id: "1", position: { x: 250, y: 5 }, data: { label: "Node 1" } },
     { id: "2", position: { x: 100, y: 100 }, data: { label: "Node 2" } },
   ]);
+  const [edges, setEdges, onEdgesChange] = useEdgesState([]);
+  const [selectedNode, setSelectedNode] = useState<Node | null>(null);
 
-  const [edges, setEdges, onEdgesChange] = useEdgesState([
-    { id: "e1-2", source: "1", target: "2" },
-  ]);
+  const onNodeClick: NodeMouseHandler = useCallback((event, node) => {
+    event.preventDefault();
+    setSelectedNode(node);
+  }, []);
 
-  // add new node
-  const addNode = useCallback(() => {
-    const newNode = {
-      id: getId(),
-      position: { x: Math.random() * 400, y: Math.random() * 400 },
-      data: { label: `Node ${id}` },
-    };
-    setNodes((nds) => nds.concat(newNode));
-  }, [setNodes]);
-
-  // delete exist node
-  const deleteNode = useCallback(
-    (nodeId: string) => {
-      setNodes((nds) => nds.filter((node) => node.id !== nodeId));
-      setEdges((eds) =>
-        eds.filter((edge) => edge.source !== nodeId && edge.target !== nodeId)
-      );
-    },
-    [setNodes, setEdges]
-  );
+  const deleteSelectedNode = useCallback(() => {
+    if (!selectedNode) return;
+    setNodes((nds) => nds.filter((node) => node.id !== selectedNode.id));
+    setEdges((eds) =>
+      eds.filter(
+        (edge) =>
+          edge.source !== selectedNode.id && edge.target !== selectedNode.id
+      )
+    );
+    setSelectedNode(null);
+  }, [selectedNode, setNodes, setEdges]);
 
   return (
-    <main className="flex flex-col items-center justify-center h-screen">
+    <div className="flex flex-col items-center justify-center h-screen">
       <div className="mb-4 flex gap-4">
         <button
           className="px-4 py-2 bg-blue-500 text-white rounded"
-          onClick={addNode}
+          onClick={() =>
+            setNodes((nds) =>
+              nds.concat({
+                id: getId(),
+                position: { x: Math.random() * 400, y: Math.random() * 400 },
+                data: { label: `Node ${id}` },
+              })
+            )
+          }
         >
           Add Node
         </button>
         <button
           className="px-4 py-2 bg-red-500 text-white rounded"
-          onClick={() => deleteNode("2")} // ID 2 のノードを削除
+          onClick={deleteSelectedNode}
+          disabled={!selectedNode}
         >
-          Delete Node 2
+          Delete Selected Node
         </button>
       </div>
       <ReactFlow
@@ -60,10 +69,11 @@ function App() {
         edges={edges}
         onNodesChange={onNodesChange}
         onEdgesChange={onEdgesChange}
+        onNodeClick={onNodeClick}
       >
         <Background />
       </ReactFlow>
-    </main>
+    </div>
   );
 }
 
