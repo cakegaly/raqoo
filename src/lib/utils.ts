5dd65df388c5a0df6641a2e47b81292425b6be45
Toshiki Okazaki <toshiki.okazaki@gree.net>
2024-12-08 18:45:06 +0900

    refactor: update App.tsx with flow-canvas component

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,9 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+let id = 2;
+
+export function getId(): string {
+  return `${++id}`;
+}
